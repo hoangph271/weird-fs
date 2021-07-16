9f44f7006cfc7100ad0a1dc45ee8c5b690da5ff0
Huy Hoang Phan <hoangph271@gmail.com>
2021-07-16 10:48:29 +0700

    [Init] Proto

--- index.js
+++ index.js
@@ -1,22 +1,126 @@
 const IPFS = require('ipfs-core')
+const express = require('express')
+// const cors = require('cors')
 
-
-;(async () => {
-  const ipfs = await IPFS.create()
-
-  try {
-    // await ipfs.add('hello.world')
-    const files = await ipfs.files.ls('/')
-
-    for await (const file of files) {
-      console.info(file)
-    }
-  } catch (error) {
-    console.error(error)
-  } finally {
-    await ipfs.stop()
-      .finally(() => {
-        process.exit(0)
-      })
+function createClientStat (stats) {
+  return {
+    ...stats,
+    cid: stats.cid.toV1().toString('base64url')
   }
-})()
+}
+
+let _ipfs
+// Promise.resolve().then((ipfs) => {
+IPFS.create({
+  repo: 'E:\\useIpfs\\.ipfs'
+}).then(async ipfs => {
+  _ipfs = ipfs
+
+  const { PORT = 8081 } = process.env
+
+  const app = express()
+
+  // app.use(cors())
+
+  app.post('/write/:path', async (req, res) => {
+    const path = decodeURIComponent(Buffer.from(req.params.path, 'base64').toString())
+
+    const buffers = []
+
+    await new Promise((resolve) => {
+      req.once('end', resolve).on('data', chunk => buffers.push(chunk))
+    })
+
+    await ipfs.files.touch(`/${path}`)
+    await ipfs.files.write(`/${path}`, Uint8Array.from(Buffer.concat(buffers)))
+
+    res.sendStatus(201)
+  })
+
+  app.post('/mkdir/:path', async (req, res) => {
+    const path = decodeURIComponent(Buffer.from(req.params.path, 'base64').toString())
+
+    await ipfs.files.mkdir(`/${path}`, { parents: true })
+
+    res.sendStatus(201)
+  })
+
+  app.get('/raw/:cid', async (req, res) => {
+    const cid = new IPFS.CID(req.params.cid)
+    // const stats = await ipfs.files.stat(cid)
+    // let range = req.range(stats.size)
+
+    // if (range === undefined) {
+    //   range = [{ start: 0, end: stats.size - 1 }]
+    //   range.type = 'bytes'
+    //   range.isFull = true
+    // }
+
+    // if (range < 0) return res.sendStatus(400)
+    // if (range.type !== 'bytes') return res.sendStatus(400)
+
+    // const [{ start, end }] = range
+
+    // res.status(range.isFull ? 200 : 206)
+    // // TODO: Content type
+    // res.setHeader('Content-Length', stats.size)
+    // res.setHeader('Accept-Ranges', 'bytes')
+    // res.setHeader('Content-Range', `bytes ${start}-${end}/${stats.size}`)
+
+    if (req.query.filename) {
+      res.setHeader('Content-Disposition', `attachment; filename*=UTF-8''${encodeURIComponent(req.query.filename)}`)
+    }
+
+    for await (const chunk of ipfs.files.read(cid/*, { start, end }*/)) {
+      res.write(chunk)
+    }
+
+    res.once('drain', () => res.end())
+  })
+
+  app.get('/dir/:path', async (req, res) => {
+    const path = decodeURIComponent(decodeURIComponent(Buffer.from(req.params.path, 'base64').toString()))
+
+    const stats = await ipfs.files.stat(path)
+    stats.path = path
+
+    if (stats.type === 'directory') {
+      stats.children = []
+
+      for await (const child of ipfs.files.ls(path)) {
+        const childStat = createClientStat(child)
+        childStat.path = `${path.endsWith('/') ? path : `${path}/`}${child.name}`
+        stats.children.push(childStat)
+      }
+    }
+
+    res.send(createClientStat(stats))
+  })
+
+  app.delete('/:path', async (req, res) => {
+    const path = decodeURIComponent(decodeURIComponent(Buffer.from(req.params.path, 'base64').toString()))
+
+    await ipfs.files.rm(path, { recursive: true })
+
+    res.send(200)
+  })
+
+  app.all('*', (_, res) => res.sendStatus(404))
+
+  app.listen(PORT, () => {
+    console.info(`http://localhost:${PORT}`)
+  })
+
+  process.once('beforeExit', async () => {
+    await ipfs.stop()
+  }).once('unhandledRejection', () => {
+  })
+}).catch(async (e) => {
+  console.error(e)
+
+  if (_ipfs) {
+    await _ipfs.stop()
+  }
+
+  process.exit(0)
+})
